d38d83ad574c8ac3e42ffbf0081d2c7fd047d2eb
rylan sedlacek <sedlacekrylan@gmail.com>
2025-02-28 15:48:36 -0500

    Add basic parsing code in rust

--- tool/src/main.rs
+++ tool/src/main.rs
@@ -1,1 +1,33 @@
+mod generate;
+mod commands;
+
+use tokio::{stdout}
+use ollama_rs::Ollama;
+use clap::{App, Arg, SubCommand}; 
+
+// TODO main func
+
+
+
+async fn logic(script_path &str) -> Result<(), Box<dyn std::error::Error>> {
+
+    // start ollama server like blv run
+
+    let out = commands::run_python_script(script_path);
+    
+    //decide what to do
+    match out {
+        Ok((commands::OutputType::Stdout, out)) => { // no issue
+            println!("{}", out);
+            Ok(())
+        }
+
+        // TODO add error handle Ok
+
+        Err(e) => { // handle error
+            eprintln!("Error: {}", e);
+            Ok(())
+        }
+    }
+}
 
