0391c9297a229ca8b3ef09f7cb6318c7d37a040c
rylan sedlacek <sedlacekrylan@gmail.com>
2025-03-31 15:38:38 -0400

    Added basic process_loop, for additional context

--- tool/src/generate.rs
+++ tool/src/generate.rs
@@ -1,48 +1,109 @@
-use ollama_rs::Ollama;
-use ollama_rs::generation::completion::request::GenerationRequest;
 use crate::commands;
+use indicatif::{ProgressBar, ProgressStyle};
+use ollama_rs::{generation::completion::{request::GenerationRequest, GenerationContext, GenerationResponseStream,},Ollama,};
 use std::error::Error;
-
-// Same function that used to be inside of main.
-// want to update this to have a generate loop as in blv run
-
-// We want to store the outputs in a json file to keep conversation history for further context
-// This is so if a user keeps working through multiple errors we can keep track of whats been accomplished already
-
-// basically I'm pretty sure blv does it to allow for multiple turn response
+use tokio::io::{AsyncWriteExt, Stdout};
+use tokio_stream::StreamExt;
 
 pub async fn process_script(script_path: &str, explain: bool, diff: bool) -> Result<(), Box<dyn Error>> {
-    commands::start_ollama_server()?; // start server
+    commands::start_ollama_server()?; // start the Ollama server
     let out = commands::run_script(script_path);
 
     match out {
-        Ok((commands::OutputType::Stdout, output)) => {
-            println!("{}", output); // just print stdout normally
-        }
-
-        Ok((commands::OutputType::Stderr, error_output)) => {
-            let ollama = Ollama::default(); // get the error
-            let model_name = "gemma:2b"; // TODO change this TO CURRENT
-
-            let prompt = format!("provide error line number. explain this error in 3-4 bullet points. 
-            just provide the bullet points and line number. :\n{}", error_output); // to mock our goal output for fine-tuned model
-
-            if explain { // USES THE DOWNLOADED MODEL (will be called blvflag_model) when done
-                let request = GenerationRequest::new(model_name.to_string(), prompt);
-                let response = ollama.generate(request).await?;
-                println!("Explanation:\n{}", response.response); 
-            } 
-
-            if diff {
-                println!("{}", script_path);
-                //TODO here we will have the diff stuff
+            Ok((commands::OutputType::Stdout, output)) => { // basic output
+                println!("{}", output);
             }
-        }
+
+            Ok((commands::OutputType::Stderr, error_output)) => { // error
+                let ollama = Ollama::default();
+                let mut context: Option<GenerationContext> = None; // store conversation context
+                let pb = setup_progress_bar(100); // and also set up progress tracking
+                let mut stdout = tokio::io::stdout(); // output
+
+                if explain { // explain flag
+                    loop {
+                        let prompt = format!(
+                            "Provide the error line number and explain the error in 3-4 bullet points. \
+                            Just provide the bullet points and line number:\n{}",
+                            error_output
+                        );
+                        process_loop(&mut stdout, &ollama, &pb, false, &prompt, "", &mut context).await?;
+                        break; // for now break, later prompt for input
+                    }
+                }
+
+                if diff { // diff flag
+                    println!("diff place holder")
+                    /*
+                        loop {
+                            let prompt = format!(
+                                "Provide the error line number and explain the error in 3-4 bullet points. \
+                                Just provide the bullet points and line number:\n{}",
+                                error_output
+                            );
+                            process_loop(&mut stdout, &ollama, &pb, false, &prompt, "", &mut context).await?;
+                            break; // for now break, later prompt for input
+                        }
+                    */
+                }
+            } // end error block
 
         Err(_) => {
-            eprintln!("Failed to print script output");
+            eprintln!("Failed to execute the script");
         }
-    } // end match
 
-    Ok(()) // done
+    } // match
+    Ok(())
 }
+
+pub fn setup_progress_bar(max_tokens: u64) -> ProgressBar { // same as used in setup
+    let pb = ProgressBar::new(max_tokens);
+    pb.set_style(
+        ProgressStyle::default_bar()
+            .template("[{elapsed_precise}] [{bar:40.red/red] {pos:>7}/{len:7} {msg}")
+            .expect("Failed to set progress bar template")
+            .progress_chars("#>-"),
+    );
+    pb // return the bar
+}
+
+pub async fn process_loop(stdout: &mut Stdout, ollama: &Ollama, pb: &ProgressBar, show_stream: bool, query: &str, postfix: &str, context: &mut Option<GenerationContext>,) -> Result<(), Box<dyn std::error::Error>> {
+    commands::start_ollama_server()?; // always start server
+    let mut final_output = Vec::new(); // store responses
+
+    stdout.flush().await?; // overwrite past output
+
+    let mut request = GenerationRequest::new("test".into(), query.to_string()); // create the request
+    if let Some(ctx) = context.clone() { // additional context
+        request = request.context(ctx);
+    }
+    let mut stream: GenerationResponseStream = ollama.generate_stream(request).await?; // begin streaming
+
+    while let Some(Ok(res)) = stream.next().await { // loop through the response strem
+        for ele in res {
+            if show_stream {
+                stdout.write_all(ele.response.as_bytes()).await?; // print
+                stdout.flush().await?;
+            } else {
+                final_output.push(ele.response);
+                pb.inc(1); // increment progress bar
+            }
+
+            if let Some(final_data) = ele.final_data {
+                *context = Some(final_data.context); // TODO this is where we will store context, overwrite once convo is done
+            }
+        }
+    } // end while
+
+    let full_output = final_output.join(""); // TODO combine stored and current for final output
+    let full_output = format!("{}{}", postfix, full_output); // append postfix, depricated from blv run
+
+    if !show_stream {
+        pb.finish_with_message("generation complete"); // what it says
+        stdout.write_all(full_output.as_bytes()).await?;
+        stdout.flush().await?;
+    }
+
+    Ok(())
+} // end loop
+
