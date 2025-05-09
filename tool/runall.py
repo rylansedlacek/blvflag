63bb70bd10888cb411e9843948484803300dbf82
rylan sedlacek <sedlacekrylan@gmail.com>
2025-05-09 13:02:56 -0400

    Just comments

--- tool/runall.py
+++ tool/runall.py
@@ -1,5 +1,15 @@
 import subprocess
 
-for i in range(20): # a simple script to run all test files for metric generation
+for i in range(5): # a simple script to run all test files for metric generation
     for j in range(1, 21):
         subprocess.run(["cargo", "run", f"/Users/rylan/blvflag/results/test_scripts/test{j}.py", "--explain"])
+
+'''
+for i in range(5): # a simple script to run all test files for metric generation
+    for j in range(1, 21):
+        subprocess.run(["cargo", "run", f"/Users/rylan/blvflag/results/test_scripts/test{j}.py", "--diff"])
+
+for i in range(5): # a simple script to run all test files for metric generation
+    for j in range(1, 21):
+        subprocess.run(["cargo", "run", f"/Users/rylan/blvflag/results/test_scripts/test{j}.py"])
+'''
