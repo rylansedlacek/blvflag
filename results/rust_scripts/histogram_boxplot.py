48d3c671e001a41ae7dfb0129830dcc293c77b06
rylan sedlacek <sedlacekrylan@gmail.com>
2025-05-17 15:27:04 -0400

    Just some metric stuff revert if needed

--- results/rust_scripts/histogram_boxplot.py
+++ results/rust_scripts/histogram_boxplot.py
@@ -6,12 +6,12 @@ df = pd.read_json("/Users/rylan/blvflag/tool/logs/timings.jsonl", lines=True) # 
 df['timestamp'] = pd.to_datetime(df['timestamp']) # read just the time stamp and translate it to python readable
 
 sns.histplot(df['duration_sec'], bins=20, kde=True)
-plt.title("Runtime Distribution With --diff")
+plt.title("Runtime Distribution With --explain")
 plt.xlabel("Duration (seconds)")
 plt.ylabel("Count")
 plt.show()
 
 sns.boxplot(x=df['duration_sec'])
-plt.title("Execution Time With --diff")
+plt.title("Execution Time With --explain")
 plt.ylabel("Duration (seconds)")
 plt.show()
